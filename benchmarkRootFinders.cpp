4a1c8363aae4affda11f41966a68739a9bcd1fc7
weberbryan40 <weber.bryan40@gmail.com>
2018-03-09 13:41:51 -0600

    Add files via upload
    Correccion clase encapsuladora de funciones

--- benchmarkRootFinders.cpp
+++ benchmarkRootFinders.cpp
@@ -6,36 +6,42 @@
 #include <cmath>
 namespace anpi {
     template <typename T>
-    class encapsulator : std::function<T(T)>  {
+    class encapsulator{
     private:
-        int f1count;
-        int f2count;
-        int f3count;
+        static int f1count;
+        static int f2count;
+        static int f3count;
     public:
-        encapsulator() {
-            f1count=0;
-            f2count=0;
-            f3count=0;
-        }
+        encapsulator() {}
         int getF1count() const { return f1count; }
         int getF2count() const { return f2count; }
         int getF3count() const { return f3count; }
+        static void setF1count(){f1count++;}
+        static void setF2count(){encapsulator::f2count++;}
+        static void setF3count(){encapsulator::f3count++;}
 
-        inline T sqr(const T x) { return x * x; }
+        static inline T sqr(const T x) { return x * x; }
 
-        T funct1(const T x) {
-            f1count++;
+        static T funct1(const T x) {
+            setF1count();
             return std::abs(x) - std::exp(-x);
         }
 
-        T funct2(const T x) {
-            f2count++;
+        static T funct2(const T x) {
+            setF2count();
             return std::exp(-x * x) - std::exp(-sqr(x - T(3)) / T(3));
         }
 
-        T funct3(const T x) {
-            f3count++;
+        static T funct3(const T x) {
+            setF3count();
             return x * x - std::atan(x);
         }
     };
+
+    template <typename T>
+    int encapsulator<T>::f1count = 0;
+    template <typename T>
+    int encapsulator<T>::f2count = 0;
+    template <typename T>
+    int encapsulator<T>::f3count = 0;
 }
