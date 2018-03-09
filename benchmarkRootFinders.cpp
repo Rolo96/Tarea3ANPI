95be569623c58de78219f6548157d8df209852f7
Ronypa <ronxrpaniaguach@gmail.com>
2018-03-09 15:22:15 -0600

    set de contadores = 0

--- benchmarkRootFinders.cpp
+++ benchmarkRootFinders.cpp
@@ -16,28 +16,27 @@ namespace anpi {
         int getF1count() const { return f1count; }
         int getF2count() const { return f2count; }
         int getF3count() const { return f3count; }
-        static void setF1count(){f1count++;}
-        static void setF2count(){encapsulator::f2count++;}
-        static void setF3count(){encapsulator::f3count++;}
+        static void setF1count(int num){f1count+=num;}
+        static void setF2count(int num){f2count+=num;}
+        static void setF3count(int num){f3count+=num;}
 
         static inline T sqr(const T x) { return x * x; }
 
         static T funct1(const T x) {
-            setF1count();
+            setF1count(1);
             return std::abs(x) - std::exp(-x);
         }
 
         static T funct2(const T x) {
-            setF2count();
+            setF2count(1);
             return std::exp(-x * x) - std::exp(-sqr(x - T(3)) / T(3));
         }
 
         static T funct3(const T x) {
-            setF3count();
+            setF3count(1);
             return x * x - std::atan(x);
         }
     };
-
     template <typename T>
     int encapsulator<T>::f1count = 0;
     template <typename T>
